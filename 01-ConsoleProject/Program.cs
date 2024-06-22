438bfbde68e88ece2108c8afb72db3e733796c5c
MarAntBQ <github@marantbq.dev>
2024-06-21 22:47:07 -0500

    Create first project

---
+++ 01-ConsoleProject/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Display a message using String Interpolation saying > Line 1 "My name is Marco Antonio" | Line 2 "I am a Full Stack Web Developer"
+string userName = "Marco Antonio";
+string career = "Full Stack Web Developer";
+Console.WriteLine("My name is " + userName);
+Console.WriteLine("I am a " +  career);
