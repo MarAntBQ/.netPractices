fe04a6bdd8a4a5827fb6f87feb48d1effbf3271f
MarAntBQ <github@marantbq.dev>
2024-06-21 23:02:13 -0500

    Create a second project

---
+++ 02-GlazerCalc/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class GlazerCalc
+{
+    static void Main()
+    {
+        double width, height, woodLength, glassArea;
+        string widthString, heightString;
+
+        widthString = Console.ReadLine();
+        width = double.Parse(widthString);
+
+        heightString = Console.ReadLine();
+        height = double.Parse(heightString);
+
+        Console.WriteLine("The width is " + width + " and the height is " + height);
+    }
+}
