3c4605371ad4481af15a6f3438865e5c1c3bf28c
MarAntBQ <github@marantbq.dev>
2024-06-24 23:39:02 -0500

    For project 2 show messages

--- 02-GlazerCalc/Program.cs
+++ 02-GlazerCalc/Program.cs
@@ -7,9 +7,11 @@ class GlazerCalc
         double width, height, woodLength, glassArea;
         string widthString, heightString;
 
+        Console.WriteLine("Please insert the Width:");
         widthString = Console.ReadLine();
         width = double.Parse(widthString);
 
+        Console.WriteLine("Please insert the Height:");
         heightString = Console.ReadLine();
         height = double.Parse(heightString);
 
--- 02-GlazerCalc/obj/Debug/net8.0/GlazerCalc.cs.AssemblyInfo.cs
+++ 02-GlazerCalc/obj/Debug/net8.0/GlazerCalc.cs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GlazerCalc.cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d897967b92def05b6f528d9c32413182757d5162")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe04a6bdd8a4a5827fb6f87feb48d1effbf3271f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GlazerCalc.cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GlazerCalc.cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
